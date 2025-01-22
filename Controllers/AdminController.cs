c5403a303b6af4a548a736a59cece8cc79fff201
AdilBaha2 <adilbaha54@gmail.com>
2025-01-23 00:59:31 +0300

    Initial commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OtelSitesi.Data;
+using OtelSitesi.Models;
+
+namespace OtelSitesi.Controllers
+{
+	public class AdminController : Controller
+	{
+		
+		private const string AdminUsername = "admin";
+		private const string AdminPassword = "123456";
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(string username, string password)
+		{
+			if (username == AdminUsername && password == AdminPassword)
+			{
+				
+				return RedirectToAction("Dashboard");
+			}
+
+			ViewData["Error"] = "Kullanıcı adı veya şifre yanlış!";
+			return View();
+		}
+
+		public IActionResult Dashboard()
+		{
+			return View();
+		}
+	}
+}
